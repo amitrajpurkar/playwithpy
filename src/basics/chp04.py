a649bb05f50f8bca0b970740b67b8987187a2151
Amit Rajpurkar <amitrajpurkar@gmail.com>
2023-09-26 19:46:27 -0400

    attempt more problems in chapter 4

--- src/basics/chp04.py
+++ src/basics/chp04.py
@@ -23,7 +23,163 @@ def tryout_exercise01():
         len_in_inches = length * 2.54
         print("the length",length," cms is equal to ",len_in_inches,"inches")
     pass
-    # sds
+
+def tryout_exercise02():
+    temp = input("give a temperature value: ")
+    unit = input("what is the unit: Celcius/ Farenheit (C or F): ")
+    converted_temp = None
+    if not temp or not temp.isnumeric() or not unit or not unit.isnumeric():
+        print("one of the inputs is not correct: ",temp, unit)
+    else:
+        temp = eval(temp)
+        if "c" == unit.lower().strip():
+            converted_temp = round(((9 / 5) * temp) + 32,3)
+            print("temperature in Farenheit = ",converted_temp)
+        elif "f" == unit.lower().strip():
+            converted_temp = round(5 / 9 * (temp - 32), 3)
+            print("temperature in Celcius = ", converted_temp)
+        else:
+            print("unknown unit of temperature",unit)
+
+def tryout_exercise03():
+    temp = input("enter a temperature in Celcius: ")
+    if not temp or not temp.isnumeric():
+        print("the input is not correct: ",temp)
+    else:
+        temp = eval(temp)
+        if temp < -273.15:
+            print("the temperature is invalid because it is below absolute zero")
+        elif temp == -275.15:
+            print("the temperature is absolute zero")
+        elif temp > -275.15 and temp < 0:
+            print("the temperature is below freezing")
+        elif temp == 0:
+            print("the temperature is at freezing point")
+        elif temp > 0 and temp < 100:
+            print("the temperature is at normal range")
+        elif temp == 100:
+            print("the temperature is at boiling point")
+        elif temp > 100:
+            print("the temperature is above boiling point")
+
+def tryout_exercise04():
+    credits = input("how many credits have you taken? ")
+    if not credits or not credits.isnumeric():
+        print("credits given is not numeric", credits)
+    else:
+        credits = int(credits)
+        if credits <= 23:
+            print("the student is a freshman")
+        elif credits > 23 and credits <= 53:
+            print("the student is a somophore")
+        elif credits > 53 and credits <= 83:
+            print("the student is a junior")
+        elif credits >= 84:
+            print("the student is a senior")
+    pass
+
+def tryout_exercise05():
+    num = randint(1, 10)
+    guess = input('Guess a number: ')
+    if not guess or not guess.isnumeric():
+        print("you did not enter a number !!",guess)
+    else:
+        guess = int(guess)
+        if guess == num:
+            print('You got it!')
+        else:
+            print('Sorry. The number is ', num)
+
+def tryout_exercise06():
+    high_price = 12 #-- if buying less than 10 items
+    medium_price = 10 #-- if buying between 10 to 99
+    low_price = 7 #-- if buying 100 or more
+    num_of_items = input("how many items are you buying? (in numbers only) ")
+    if not num_of_items or not num_of_items.isnumeric():
+        print("enter numeric value",num_of_items)
+    else:
+        num_of_items = int(num_of_items)
+        if num_of_items < 10:
+            print("total cost = $",round(high_price * num_of_items),2)
+        elif num_of_items >=10 and num_of_items <=99:
+            print("total cost = $",round(medium_price * num_of_items),2)
+        elif num_of_items >= 100:
+            print("total cost = $",round(low_price * num_of_items),2)
+    pass
+
+def tryout_exercise07():
+    x, y = input("enter two numbers ").split()
+    if not x or not x.isnumeric() or not y or not y.isnumeric():
+        print("input is not numeric", x, y)
+    else:
+        if abs(x - y) <= 0.001:
+            print("close")
+        else:
+            print("not close")
+    pass
+
+def tryout_exercise08():
+    Y = input("enter a year (format=yyyy): ")
+    if not Y or not Y.isnumeric() or Y.isspace():
+        print("you did not enter year number")
+    else:
+        Y = eval(Y)
+        isDivisibleBy4 = (Y % 4 == 0)
+        isDivisibleBy100 = (Y % 100 == 0)
+        isDivisibleBy400 = (Y % 400 == 0)
+
+        if isDivisibleBy400:
+            # for example 2000
+            print(Y, "is a Leap Year")
+        elif isDivisibleBy100:
+            #  for example 1900
+            print(Y, "is not a Leap Year")
+        elif isDivisibleBy4:
+            # for example 2008
+            print(Y, "is a Leap Year")
+        else:
+            print(Y, "is not a Leap Year")
+    pass
+
+def tryout_exercise09():
+    n = input("give a number: ")
+    n = eval(n)
+    for i in range(1, int(n / 2) + 1):
+        if n % i == 0:
+            # yield i
+            print(i)
+    # yield n
+    print(n)
+    pass
+
+def tryout_exercise10():
+    pass
+
+def tryout_exercise11():
+    hour = input("Enter hour: ")
+    am_pm = eval(input("am (1) or pm (2)? "))
+    hrs_ahead = input("How many hours ahead? ")
+
+    if hour.isnumeric() and hrs_ahead.isnumeric():
+        total_hrs = eval(hour) + eval(hrs_ahead)
+        if total_hrs > 12:
+            hrs_after12 = total_hrs % 12
+            if am_pm == 1: print("New hour:", hrs_after12, "pm")
+            elif am_pm == 2: print("New hour:", hrs_after12, "am")
+            else: print("should not get here")
+        else:
+            if am_pm == 1: print("New hour:", total_hrs, "am")
+            elif am_pm == 2: print("New hour:", total_hrs, "pm")
+            else: print("should not get here")
+    else:
+        print("one of the inputs was not numeric", hour, hrs_ahead, am_pm)
+    pass
+
+def tryout_exercise12():
+    pass
+
+def tryout_exercise13():
+    pass
 
 
 def selectFromDict(options, name):
@@ -43,6 +199,24 @@ def selectFromDict(options, name):
             print('Selected ' +  name + ': ' + str(selected))
             if "exercise01" == selected:
                 tryout_exercise01()
+            elif "exercise02" == selected:
+                tryout_exercise02()
+            elif "exercise03" == selected:
+                tryout_exercise03()
+            elif "exercise04" == selected:
+                tryout_exercise04()
+            elif "exercise05" == selected:
+                tryout_exercise05()
+            elif "exercise06" == selected:
+                tryout_exercise06()
+            elif "exercise07" == selected:
+                tryout_exercise07()
+            elif "exercise08" == selected:
+                tryout_exercise08()
+            elif "exercise09" == selected:
+                tryout_exercise09()
+            elif "exercise11" == selected:
+                tryout_exercise11()
             elif "example01" == selected:
                 guess_a_number()
             else:
@@ -63,18 +237,18 @@ def selectFromDict(options, name):
 options = {}
 #     [USER OPTION] = PROGRAM RESULT
 options['01. Given length in cms, give length in inches'] = "exercise01"
-options['02. Give and hour and number of hrs, give what will the time in oclock'] = "exercise02"
-options['03. Given a number, tell last x digits of that number'] = "exercise03"
-options['04. Given weight in kgs, give the weight in pounds'] = "exercise04"
-options['05. Given a number, tell the factorial of that'] = "exercise05"
-options['06. Given a number, tell the sin, cos, tan of that number'] = "exercise06"
-options['07. Given an angle in degrees, tell sine of that angle'] = "exercise07"
-options['08. Tell sin, cos of angles from 0 to 345, increments of 15'] = "exercise08"
-options['09. Given an year, find the date of Easter in that year'] = "exercise09"
-options['10. Given an year, tell if its a Leap year or not'] = "exercise10"
-options['11. Given an amount, tell how many quaters, dimes, nickels and pennies'] = "exercise11"
-options['12. Given height and width, print a modular rectangle'] = "exercise12"
-options['13. Given height and width, print a modular rectangle'] = "exercise13"
+options['02. given a temperature and unit, convert into other unit'] = "exercise02"
+options['03. Given a temp in celcius, tel what range it is in'] = "exercise03"
+options['04. Given credits, tell which grade the student is in'] = "exercise04"
+options['05. Guess a number between 1 to 10'] = "exercise05"
+options['06. Given number of items, tell the total cost'] = "exercise06"
+options['07. given two numbers, tell if they are close or not'] = "exercise07"
+options['08. Given an year, tell if its a Leap year or not'] = "exercise08"
+options['09. Given a number, tell all its divisors'] = "exercise09"
+options['10. ask 10 multiplications, check if answered right or wrong'] = "exercise10"
+options['11. print how many hours in future'] = "exercise11"
+options['12. guess candies in a bowl'] = "exercise12"
+options['13. rock paper scisors 5 rounds'] = "exercise13"
 options['eg1. Guess a random number'] = "example01"
 
 
