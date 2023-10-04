2bb6ce441ffb664b15a9df3cf5b8933aa9731e11
Amit Rajpurkar <amitrajpurkar@gmail.com>
2023-10-04 19:37:47 -0400

    updates on rock-paper-scissor problem

--- src/basics/chp04.py
+++ src/basics/chp04.py
@@ -228,17 +228,40 @@ def tryout_exercise12():
 
 def tryout_exercise13():
     print("program that lets the user play Rock-Paper-Scissors against the computer")
+    rock = 1
+    paper = 2
+    scissor = 3
+    # paper > rock > scissor > paper
+
     user_wins = 0
     computer_wins = 0
+    draws = 0
     for i in range(5):
         compPick = randint(1,3) ## 1=rock; 2=paper; 3=scissor
         uPick = input("enter your number: 1.rock, 2.paper, 3.scissor: ")
         userPick = eval(uPick) if uPick.isnumeric() else 0
-        if userPick == compPick:
-            user_wins = user_wins + 1
-        else:
-            computer_wins = computer_wins + 1
-    print("Results -- user wins",user_wins,"times and computer wins",computer_wins,"times")
+        if compPick == rock:
+            if userPick == rock:
+                draws = draws + 1
+            elif userPick == paper:
+                user_wins = user_wins + 1
+            elif userPick == scissor:
+                computer_wins = computer_wins + 1
+        elif compPick == paper:
+            if userPick == rock:
+                computer_wins = computer_wins + 1
+            elif userPick == paper:
+                draws = draws + 1
+            elif userPick == scissor:
+                user_wins = user_wins + 1
+        elif compPick == scissor:
+            if userPick == rock:
+                user_wins = user_wins + 1
+            elif userPick == paper:
+                computer_wins = computer_wins + 1
+            elif userPick == scissor:
+                draws = draws + 1
+    print("Results -- user wins",user_wins,"times, computer wins",computer_wins,"times, and draws",draws,"times")
     pass
 
 
