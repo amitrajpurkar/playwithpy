16220713062987fd0c36f3bfcd67d24692303569
Amit Rajpurkar <amitrajpurkar@gmail.com>
2023-10-04 20:09:28 -0400

    adding another variation for rock-paper-scissor

--- src/basics/chp04.py
+++ src/basics/chp04.py
@@ -237,7 +237,7 @@ def tryout_exercise13():
     computer_wins = 0
     draws = 0
     for i in range(5):
-        compPick = randint(1,3) ## 1=rock; 2=paper; 3=scissor
+        compPick = randint(1,3)
         uPick = input("enter your number: 1.rock, 2.paper, 3.scissor: ")
         userPick = eval(uPick) if uPick.isnumeric() else 0
         if compPick == rock:
@@ -264,6 +264,34 @@ def tryout_exercise13():
     print("Results -- user wins",user_wins,"times, computer wins",computer_wins,"times, and draws",draws,"times")
     pass
 
+def tryout_exercise13_v2():
+    print("program that lets the user play Rock-Paper-Scissors against the computer")
+    rock = 1
+    paper = 2
+    scissor = 3
+    # paper > rock > scissor > paper
+
+    user_wins = 0
+    computer_wins = 0
+    draws = 0
+    for i in range(5):
+        compPick = randint(1,3)
+        uPick = input("enter your number: 1.rock, 2.paper, 3.scissor: ")
+        userPick = eval(uPick) if uPick.isnumeric() else 0
+        if ((compPick == rock and userPick == rock)
+            or (compPick == paper and userPick == paper)
+            or (compPick == scissor and userPick == scissor)):
+                draws = draws + 1
+        elif ((compPick == rock and userPick == paper)
+            or (compPick == paper and userPick == scissor)
+            or (compPick == scissor and userPick == rock)):
+                user_wins = user_wins + 1
+        elif ((compPick == rock and userPick == scissor)
+            or (compPick == paper and userPick == rock)
+            or (compPick == scissor and userPick == paper)):
+                computer_wins = computer_wins + 1
+    print("Results -- user wins",user_wins,"times, computer wins",computer_wins,"times, and draws",draws,"times")
+    pass
 
 def selectFromDict(options, name):
     index = 0
