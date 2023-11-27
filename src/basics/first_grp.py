fba35db71f4ea4c1a9f017c67597be99500d251e
Amit Rajpurkar <amitrajpurkar@gmail.com>
2023-11-26 21:03:38 -0500

    first try at graphics

---
+++ src/basics/first_grp.py
@@ -0,0 +1,15 @@
+from graphics import *
+
+
+def main():
+  win = GraphWin("My Circle", 500, 500)
+  c = Circle(Point(100,100), 40)
+  c.setOutline("#ccff99")
+  c.setWidth(10)
+  c.setFill("#ffcc00")
+  c.draw(win)
+  win.getMouse() # pause for click in window
+  win.close()
+
+
+main()
